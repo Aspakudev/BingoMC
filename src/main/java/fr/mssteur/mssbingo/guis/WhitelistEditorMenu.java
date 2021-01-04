57702df790f0ad5bb06028809211ef10ce05b02e
MSSTEUR <72036887+MSSTEUR@users.noreply.github.com>
2021-01-04 17:43:22 +0100

    Deleted unused import in WhitelistEditorMenu

--- src/main/java/fr/mssteur/mssbingo/guis/WhitelistEditorMenu.java
+++ src/main/java/fr/mssteur/mssbingo/guis/WhitelistEditorMenu.java
@@ -10,11 +10,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class WhitelistEditorMenu implements InventoryProvider {
 
